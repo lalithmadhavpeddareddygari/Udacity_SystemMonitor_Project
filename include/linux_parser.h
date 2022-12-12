da586fadbc3956d6ee4d2b3df7ffbb9537a16709
Lalith Madhav Peddareddygari <107163982+lalithmadhavpeddareddygari@users.noreply.github.com>
2022-12-12 11:48:58 -0500

    Update linux_parser.h

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -43,7 +43,7 @@ enum CPUStates {
 std::vector<std::string> CpuUtilization();
 long Jiffies();
 long ActiveJiffies();
-long ActiveJiffies(int pid);
+float ActiveJiffies(int pid);
 long IdleJiffies();
 
 // Processes
